5fc95fc12b2ae3a46b23ae6a98cd33ebc5f0a07f
Elvira Faizullina <elvira0921@gmail.com>
2025-08-31 19:03:46 +0300

    create pytest --language=fr test_items.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,39 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+    language_name = request.config.getoption("--language")
+    language = None
+    if language_name == "ru":
+        print("\nthe website opens in Russian.")
+        options = Options()
+        options.add_experimental_option("prefs", {"intl.accept_languages": "ru"})
+        browser = webdriver.Chrome(options=options)
+    elif language_name == "en":
+        print("\nthe website opens in English.")
+        options = Options()
+        options.add_experimental_option("prefs", {"intl.accept_languages": "en"})
+        browser = webdriver.Chrome(options=options)
+    elif language_name == "es":
+        print("\nthe website opens in Spain.")
+        options = Options()
+        options.add_experimental_option("prefs", {"intl.accept_languages": "es"})
+        browser = webdriver.Chrome(options=options)
+    elif language_name == "fr":
+        print("\nthe website opens in French.")
+        options = Options()
+        options.add_experimental_option("prefs", {"intl.accept_languages": "fr"})
+        browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--language must be selected")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,17 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import pytest
 
+
+class TestRegistration:
+    def test_registration1(self, browser):
+        # open browser
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        browser.get(link)
+
+        # find basket button
+        button_basket = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+        time.sleep(30)
+        assert button_basket, "The button was not found"
